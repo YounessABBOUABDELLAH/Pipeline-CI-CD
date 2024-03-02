c0432e6d0a5a6751687443c21d74969597abf9fa
YounessABBOUABDELLAH <149901593+YounessABBOUABDELLAH@users.noreply.github.com>
2024-03-02 22:31:33 +0100

    Update fichierInfo.java

--- src/main/java/entities/fichierInfo.java
+++ src/main/java/entities/fichierInfo.java
@@ -16,17 +16,6 @@ public class fichierInfo {
 			this.filename = filename;
 			this.url = url;
 		}
-		public String getFilename() {
-			return filename;
-		}
-		public void setFilename(String filename) {
-			this.filename = filename;
-		}
-		public String getUrl() {
-			return url;
-		}
-		public void setUrl(String url) {
-			this.url = url;
-		}
+		
 	
 }
