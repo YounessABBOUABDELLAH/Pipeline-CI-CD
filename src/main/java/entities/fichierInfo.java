dfbec5bdd810571e7a4c9ab380739c1bd2666ede
YounessABBOUABDELLAH <149901593+YounessABBOUABDELLAH@users.noreply.github.com>
2024-03-06 13:46:22 +0100

    Update fichierInfo.java

--- src/main/java/entities/fichierInfo.java
+++ src/main/java/entities/fichierInfo.java
@@ -1,1 +1,16 @@
+package entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class fichierInfo {
+	  private String filename;
+	    private String url;
+	
+	
+}
